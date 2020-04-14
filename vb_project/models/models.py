526e8cb3286f7971429f43c107ec8b0be8961f91
nisaasila17 <nisaasila17@gmail.com>
2020-04-14 22:53:25 +0800

    Added invoice in project

--- vb_project/models/models.py
+++ vb_project/models/models.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
-    project_id = fields.Many2one(comodel_name='project.project', string='Project ID',ondelete='cascade',index=True, store=True)
+    project_id = fields.Many2one(comodel_name='project.project', string='Project ID',index=True, store=True)
     project_name = fields.Char(string = 'Project', related='project_id.name',store=True)
     
 
@@ -22,11 +22,18 @@ class SaleOrder(models.Model):
         return invoice_vals
 
 
-
-
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
     
-    sale_id = fields.Many2one(comodel_name='sale.order', string='Sale id',ondelete='cascade',index=True)
+    sale_id = fields.Many2one(comodel_name='sale.order', string='Sale id',index=True)
     project_id = fields.Many2one('project.project', string='Project ID')
-    
+
+
+class ProjectInvoice(models.Model):
+    _inherit = 'project.project'
+
+    invoice_ids = fields.One2many('account.invoice','project_id', string='Invoices')
+    payment_term_id = fields.Many2one('account.payment.term', string='Payment Terms', oldname='payment_term')
+    # invoice_id = fields.Many2many("account.invoice", string='Invoices', compute="_get_invoiced", readonly=True, copy=False)
+
+
